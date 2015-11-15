52648ec7c6abd347d1b30be212fef0c3500ef02c
schiiter <zhu302@purdue.edu>
2015-11-14 23:11:44 -0500

    aa

--- src/CircularBuffer.java
+++ src/CircularBuffer.java
@@ -2,6 +2,26 @@
  * Created by michaelzhu on 11/14/15.
  */
 public class CircularBuffer {
-int j;
-int i;
-yoyo}
+    private final int size;
+    private int count;
+
+    public CircularBuffer(int size) {
+        this.size = size;
+        this.count = 0;
+        String[] messages = new String[size];
+    }
+
+    public void put(String message) {
+        String addon;
+        addon = Integer.toString(count);
+        while (addon.length() < 4) {
+            addon = "0" + addon;
+        }
+        if(count < 9999) {
+            count++;
+        }
+        else {
+            count = 0;
+        }
+    }
+}
