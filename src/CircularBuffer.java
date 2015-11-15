ece1f4da2e3eebe570de080bb63a1afb21969185
schiiter <zhu302@purdue.edu>
2015-11-15 04:36:14 -0500

    aa

--- src/CircularBuffer.java
+++ src/CircularBuffer.java
@@ -1,14 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by michaelzhu on 11/14/15.
  */
 public class CircularBuffer {
     private final int size;
     private int count;
+    private List<String> messages;
 
     public CircularBuffer(int size) {
         this.size = size;
         this.count = 0;
-        String[] messages = new String[size];
+        this.messages = new ArrayList<>();
     }
 
     public void put(String message) {
@@ -23,5 +27,15 @@ public class CircularBuffer {
         else {
             count = 0;
         }
+
+
+        messages.add(0, addon + message);
+        if(messages.size() > size) {
+            messages.remove(size);
+        }
+    }
+
+    public String[] getNewest(int numMessages) {
+        return null;
     }
 }
